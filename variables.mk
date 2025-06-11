e65ec57cd210102ac596d1d85f274c63c72787e7
IsharaNawa <yc.ishara@gmail.com>
2025-06-11 22:54:51 +1000

    testing my fork

--- variables.mk
+++ variables.mk
@@ -71,7 +71,7 @@ ifeq ($(SUB_PROJECT),chipyard)
 	MODEL             ?= TestHarness
 	VLOG_MODEL        ?= $(MODEL)
 	MODEL_PACKAGE     ?= chipyard.harness
-	CONFIG            ?= RocketConfig
+	CONFIG            ?= FourRocketConfig
 	CONFIG_PACKAGE    ?= $(SBT_PROJECT)
 	GENERATOR_PACKAGE ?= $(SBT_PROJECT)
 	TB                ?= TestDriver
